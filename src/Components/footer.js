a9932c37295a8a6522dccaabb0de1b1b7eed0bb5
Anshika Gupta <gupta.anshika3010@gmail.com>
2020-11-19 03:38:34 +0530

    Delete footer.js

--- src/Components/footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Prod from "./prod.js";
-import Products from "./contact";
-
-function App() {
-  const prodar = Products.map((item) => <Prod key={item.id} product={item} />);
-  return <div>{prodar}</div>;
-}
-export default App;
